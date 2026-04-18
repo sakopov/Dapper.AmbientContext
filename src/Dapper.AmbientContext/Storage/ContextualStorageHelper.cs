b74260b7e6a154dea454ab1ca4c0fb60ac2bc3bc
Sergei Akopov <sergei.akopov@mediware.com>
2026-04-18 01:04:23 -0700

    docs: update copyright year to 2016-2026 in source files

--- src/Dapper.AmbientContext/Storage/ContextualStorageHelper.cs
+++ src/Dapper.AmbientContext/Storage/ContextualStorageHelper.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ContextualStorageHelper.cs">
-//   Copyright (c) 2016 Sergey Akopov
+//   Copyright (c) 2016-2026 Sergey Akopov
 //   
 //   Permission is hereby granted, free of charge, to any person obtaining a copy
 //   of this software and associated documentation files (the "Software"), to deal
